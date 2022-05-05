fdad4008ef44ca09a7030fd42908e7c39bc47ba2
duttrohan0302 <duttrohan0302@gmail.com>
2022-05-05 22:28:36 +0530

    quiz-js

--- 22. 05-05-2022/quiz/script.js
+++ 22. 05-05-2022/quiz/script.js
@@ -32,3 +32,74 @@ const quizData = [
         correct: "b",
     },
 ];
+
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question")
+
+const submitBtn = document.getElementById("submit")
+
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+
+let currentQuestionIndex = 0 //index
+
+function deselectAnswers(){
+    answerEls.forEach(answerEl=>answerEl.checked=false)
+}
+function loadQuestion(){
+
+    deselectAnswers()
+    const currentQuesData = quizData[currentQuestionIndex]
+
+    questionEl.innerText = currentQuesData.question
+
+    a_text.innerText = currentQuesData.a
+    b_text.innerText = currentQuesData.b
+    c_text.innerText = currentQuesData.c
+    d_text.innerText = currentQuesData.d
+}
+
+
+loadQuestion()
+
+let score = 0;
+
+function selectAns(){
+    let answer;
+
+    answerEls.forEach(answerEl=>{
+        if(answerEl.checked){
+            answer = answerEl.id
+        }
+    })
+    return answer;
+
+}
+submitBtn.addEventListener("click",()=>{
+
+    const answer = selectAns()
+
+    if(answer){
+        if(answer===quizData[currentQuestionIndex].correct){
+            score++;
+        }
+        currentQuestionIndex++;
+        if(currentQuestionIndex<quizData.length){
+            loadQuestion()
+        }else{
+            // quiz has ended, show the score
+            quiz.innerHTML=`
+            <h2>
+                You answered ${score}/${quizData.length} questions correctly
+
+            </h2>
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }else{
+        alert("Please select an option")
+    }
+})
